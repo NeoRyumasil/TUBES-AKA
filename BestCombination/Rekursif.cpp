d8b02c27ef8e89001897c3bbe461247c49ad8bcf
Ghanifhakbar <ghanifkuliahakbar@gmail.com>
2024-12-29 17:18:14 +0700

    Update Rekursif.cpp

--- BestCombination/Rekursif.cpp
+++ BestCombination/Rekursif.cpp
@@ -4,132 +4,106 @@
 
 using namespace std;
 
-int findWeaponIndex(const weapon &W) {
-    for (int i = 0; i < weaponCount; ++i) {
-        if (weaponList[i].itemName == W.itemName) {
-            return i;
-        }
+int weaponCount = 0;
+int armorCount = 0;
+int accessoryCount = 0;
+
+// Global variables untuk menyimpan kombinasi terbaik
+float bestCombination = 0.0f;
+string bestWeapon, bestArmor, bestAccessory;
+float finalATK, finalDEF, finalHP;
+
+void insertWeapon(weapon WEA) {
+    if (weaponCount < 10) {
+        weaponList[weaponCount++] = WEA;
+    } else {
+        cout << "Weapon list is full!" << endl;
     }
-    return -1; 
 }
 
-int findArmorIndex(const armor &A) {
-    for (int i = 0; i < armorCount; ++i) {
-        if (armorList[i].itemName == A.itemName) {
-            return i;
-        }
+void insertArmor(armor ARM) {
+    if (armorCount < 10) {
+        armorList[armorCount++] = ARM;
+    } else {
+        cout << "Armor list is full!" << endl;
     }
-    return -1;
 }
 
-int findAccessoryIndex(const accessory &A) {
-    for (int i = 0; i < accessoryCount; ++i) {
-        if (accessoryList[i].itemName == A.itemName) {
-            return i;
-        }
+void insertAccessory(accessory ACC) {
+    if (accessoryCount < 10) {
+        accessoryList[accessoryCount++] = ACC;
+    } else {
+        cout << "Accessory list is full!" << endl;
     }
-    return -1; 
 }
 
-float hitungKekuatanCharacter(int indexWeapon, int indexArmor, int indexAccessory, character &baseStats){
-    if (indexWeapon < 0 || indexAccessory < 0 || indexArmor < 0){
-        return 0;
+void findBestCombinationRecursive(int w, int a, int acc, character &baseChar) {
+    // Base case: jika sudah mencoba semua kombinasi
+    if (w >= weaponCount) {
+        return;
     }
 
-    character modifiedStats = baseStats;
+    // Hitung nilai kombinasi saat ini
+    float currentATK = weaponList[w].baseAttack + accessoryList[acc].ATK;
+    float currentDEF = armorList[a].baseDefend + accessoryList[acc].DEF;
+    float currentHP = accessoryList[acc].HP + armorList[a].baseHP;
+    float currentCombination = currentATK + currentDEF + currentHP;
 
-    if (indexWeapon >= 0){
-        modifiedStats.baseATK += weaponList[indexWeapon].baseATK;
+    // Update kombinasi terbaik jika ditemukan yang lebih baik
+    if (currentCombination > bestCombination) {
+        bestCombination = currentCombination;
+        bestWeapon = weaponList[w].itemName;
+        bestArmor = armorList[a].itemName;
+        bestAccessory = accessoryList[acc].itemName;
+        finalATK = currentATK;
+        finalDEF = currentDEF;
+        finalHP = currentHP;
     }
 
-    if (indexArmor >= 0){
-        modifiedStats.baseDEF += armorList[indexArmor].baseDEF;
-        modifiedStats.baseHP += armorList[indexArmor].baseHP;
+    // Rekursif untuk mencoba kombinasi berikutnya
+    if (acc + 1 < accessoryCount) {
+        // Coba accessory berikutnya
+        findBestCombinationRecursive(w, a, acc + 1, baseChar);
+    } else if (a + 1 < armorCount) {
+        // Jika sudah mencoba semua accessory, pindah ke armor berikutnya
+        findBestCombinationRecursive(w, a + 1, 0, baseChar);
+    } else {
+        // Jika sudah mencoba semua armor, pindah ke weapon berikutnya
+        findBestCombinationRecursive(w + 1, 0, 0, baseChar);
     }
-
-    if (indexAccessory >= 0){
-        modifiedStats.baseATK += accessoryList[indexAccessory].ATK;
-        modifiedStats.baseDEF += accessoryList[indexAccessory].DEF;
-        modifiedStats.baseHP += accessoryList[indexAccessory].HP;
-    }
-
-    return modifiedStats.baseATK + modifiedStats.baseDEF + modifiedStats.baseHP;
 }
 
-pack kombinasiTerbaikRekursif(int WEA, int ARM, int ACC, character &baseStat) {
-    float bestPower = 0;
-    pack bestCombinationPack;
+void startRecursiveSearch(character &baseChar) {
+    // Reset nilai global
+    bestCombination = 0.0f;
+    bestWeapon = "";
+    bestArmor = "";
+    bestAccessory = "";
 
-    if (WEA < 0 && ARM < 0 && ACC < 0) {
-        return bestCombinationPack;
-    }
+    // Mulai pencarian rekursif dari indeks 0
+    findBestCombinationRecursive(0, 0, 0, baseChar);
 
-    float currentPower = 0;
-    weapon currentWeapon;
-    armor currentArmor;
-    accessory currentAccessory;
+    // Output hasil
+    cout << "\n=== Before Power-Up ===\n";
+    cout << "ATK: " << baseChar.baseATK << "\n";
+    cout << "DEF: " << baseChar.baseDEF << "\n";
+    cout << "HP: " << baseChar.baseHP << "\n";
 
-    if (WEA >= 0) {
-        currentPower += weaponList[WEA].baseATK;
-        currentWeapon = weaponList[WEA];
-    }
+    cout << "\n=== After Power-Up ===\n";
+    cout << "ATK: " << finalATK + baseChar.baseATK << "\n";
+    cout << "DEF: " << finalDEF + baseChar.baseDEF << "\n";
+    cout << "HP: " << finalHP + baseChar.baseHP << "\n";
 
-    if (ARM >= 0) {
-        currentPower += armorList[ARM].baseDEF + armorList[ARM].baseHP;
-        currentArmor = armorList[ARM];
-    }
-
-    if (ACC >= 0) {
-        currentPower += accessoryList[ACC].ATK + accessoryList[ACC].DEF + accessoryList[ACC].HP;
-        currentAccessory = accessoryList[ACC];
-    }
-
-    if (currentPower > bestPower) {
-        bestPower = currentPower;
-        bestCombinationPack.WEA = currentWeapon;
-        bestCombinationPack.ARM = currentArmor;
-        bestCombinationPack.ACC = currentAccessory;
-    }
-
-    if (WEA >= 0) {
-        pack bestWeaponCombination = kombinasiTerbaikRekursif(WEA - 1, ARM, ACC, baseStat);
-        int weaponIndex = findWeaponIndex(bestWeaponCombination.WEA);
-        int armorIndex = findArmorIndex(bestWeaponCombination.ARM);
-        int accessoryIndex = findAccessoryIndex(bestWeaponCombination.ACC);
-
-        float weaponPower = hitungKekuatanCharacter(weaponIndex, armorIndex, accessoryIndex, baseStat);
-        if (weaponPower > bestPower) {
-            bestCombinationPack = bestWeaponCombination;
-            bestPower = weaponPower;
-        }
-    }
-
-    if (ARM >= 0) {
-        pack bestArmorCombination = kombinasiTerbaikRekursif(WEA, ARM - 1, ACC, baseStat);
-        int weaponIndex = findWeaponIndex(bestArmorCombination.WEA);
-        int armorIndex = findArmorIndex(bestArmorCombination.ARM);
-        int accessoryIndex = findAccessoryIndex(bestArmorCombination.ACC);
-
-        float armorPower = hitungKekuatanCharacter(weaponIndex, armorIndex, accessoryIndex, baseStat);
-        if (armorPower > bestPower) {
-            bestCombinationPack = bestArmorCombination;
-            bestPower = armorPower;
-        }
-    }
-
-    if (ACC >= 0) {
-        pack bestAccessoryCombination = kombinasiTerbaikRekursif(WEA, ARM, ACC - 1, baseStat);
-        int weaponIndex = findWeaponIndex(bestAccessoryCombination.WEA);
-        int armorIndex = findArmorIndex(bestAccessoryCombination.ARM);
-        int accessoryIndex = findAccessoryIndex(bestAccessoryCombination.ACC);
-
-        float accessoryPower = hitungKekuatanCharacter(weaponIndex, armorIndex, accessoryIndex, baseStat);
-        if (accessoryPower > bestPower) {
-            bestCombinationPack = bestAccessoryCombination;
-            bestPower = accessoryPower;
-        }
-    }
-
-
-    return bestCombinationPack;
+    cout << "\n=== Strongest Combination ===\n";
+    cout << "Character get power-up: " << endl;
+    cout << "ATTACK: " << finalATK << endl;
+    cout << "DEFENSE: " << finalDEF << endl;
+    cout << "HEALTH: " << finalHP << endl;
+    cout << "Total Value: " << bestCombination << "\n";
+    cout << endl;
+    cout << "Items: " << endl;
+    cout << "Weapon: " << bestWeapon << "\n";
+    cout << "Armor: " << bestArmor << "\n";
+    cout << "Accessory: " << bestAccessory << "\n";
 }
+
