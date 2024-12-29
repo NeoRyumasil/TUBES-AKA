e3eecffb1132c826424820774549d0e920309e2e
Ghanifhakbar <ghanifkuliahakbar@gmail.com>
2024-12-29 13:23:57 +0700

    iteratif.cpp

--- BestCombination/iteratif.cpp
+++ BestCombination/iteratif.cpp
@@ -1,12 +1,16 @@
+#include "Functional.h"
+#include "itemData.h"
+#include <iostream>
+
 void findBestCombinationIterative(int weaponCount, int armorCount, int accessoryCount, character &baseChar) {
     // Reset semua nilai global
-    bestCombination = 0.0f;
-    bestWeapon = "";
-    bestArmor = "";
-    bestAccessory = "";
-    finalATK = 0.0f;
-    finalDEF = 0.0f;
-    finalHP = 0.0f;
+    float bestCombination = 0.0f;
+    string bestWeapon = "";
+    string bestArmor = "";
+    string bestAccessory = "";
+    float finalATK = 0.0f;
+    float finalDEF = 0.0f;
+    float finalHP = 0.0f;
 
     float currentATK, currentDEF, currentHP;
 
@@ -14,17 +18,20 @@ void findBestCombinationIterative(int weaponCount, int armorCount, int accessory
     for (int w = 0; w < weaponCount; ++w) {
         for (int a = 0; a < armorCount; ++a) {
             for (int acc = 0; acc < accessoryCount; ++acc) {
-                currentATK = weaponList[w].baseAttack + accessoryList[acc].ATK;
-                currentDEF = armorList[a].baseDefend + accessoryList[acc].DEF;
+                // Hitung atribut total untuk kombinasi saat ini
+                currentATK = weaponList[w].baseATK + accessoryList[acc].ATK;
+                currentDEF = armorList[a].baseDEF + accessoryList[acc].DEF;
                 currentHP = accessoryList[acc].HP + armorList[a].baseHP;
 
+                // Evaluasi kombinasi saat ini
                 float currentCombination = currentATK + currentDEF + currentHP;
 
+                // Periksa apakah kombinasi ini lebih baik dari yang sebelumnya
                 if (currentCombination > bestCombination) {
                     bestCombination = currentCombination;
-                    bestAccessory = accessoryList[acc].itemName;
                     bestWeapon = weaponList[w].itemName;
                     bestArmor = armorList[a].itemName;
+                    bestAccessory = accessoryList[acc].itemName;
                     finalATK = currentATK;
                     finalDEF = currentDEF;
                     finalHP = currentHP;
@@ -35,14 +42,14 @@ void findBestCombinationIterative(int weaponCount, int armorCount, int accessory
 
     // Output hasil
     cout << "Before power-up:" << endl;
-    cout << "ATK: " << baseChar.ATK << endl;
-    cout << "DEF: " << baseChar.DEF << endl;
-    cout << "HP: " << baseChar.HP << endl;
+    cout << "ATK: " << baseChar.baseATK << endl;
+    cout << "DEF: " << baseChar.baseDEF << endl;
+    cout << "HP: " << baseChar.baseHP << endl;
 
     cout << "\nAfter power-up:" << endl;
-    cout << "ATK: " << finalATK + baseChar.ATK << endl;
-    cout << "DEF: " << finalDEF + baseChar.DEF << endl;
-    cout << "HP: " << finalHP + baseChar.HP << endl;
+    cout << "ATK: " << finalATK + baseChar.baseATK << endl;
+    cout << "DEF: " << finalDEF + baseChar.baseDEF << endl;
+    cout << "HP: " << finalHP + baseChar.baseHP << endl;
 
     // Menampilkan kombinasi terkuat
     cout << "\nStrongest Combination: " << bestWeapon << ", "
