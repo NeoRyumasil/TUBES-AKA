dbd4aa19fbcb73e50d154f24a71df332924ef32e
Ghanifhakbar <ghanifkuliahakbar@gmail.com>
2024-12-29 16:05:40 +0700

    Update ItemData.cpp

--- BestCombination/ItemData.cpp
+++ BestCombination/ItemData.cpp
@@ -33,7 +33,7 @@ void insertAccessory(accessory ACC, int totalAccessory){
     }
 }
 
-void data1(character &charaStats) {
+void data3(character &charaStats) {
     charaStats.baseATK = 500;
     charaStats.baseDEF = 100;
     charaStats.baseHP = 1200;
@@ -71,5 +71,56 @@ insertAccessory({"Cloak of Shadows", 80, 700, 400});
 insertAccessory({"Necklace of Power", 200, 500, 350});
 insertAccessory({"Ring of Agility", 130, 450, 280}); // Data tambahan 1
 insertAccessory({"Charm of Resilience", 110, 550, 320}); // Data tambahan 2
+
+}
+void data2(character &charaStats) {
+    charaStats.baseATK = 500;
+    charaStats.baseDEF = 100;
+    charaStats.baseHP = 1200;
+
+    // Tambahkan data senjata
+    // Tambahkan data senjata
+insertWeapon({"Laevatein", 631});
+insertWeapon({"Excalibur", 544});
+insertWeapon({"Thunder Sword", 720});
+insertWeapon({"Flame Blade", 675});
+
+// Tambahkan data armor
+insertArmor({"Aquarius Imperial Armor", 250, 1606});
+insertArmor({"Aries Bustier", 370, 1133});
+insertArmor({"Knight Plate", 450, 1400});
+insertArmor({"Golden Shield", 320, 1230});
+
+
+// Tambahkan data aksesori
+insertAccessory({"Euclase Earth", 188, 857, 300});
+insertAccessory({"Neptune Malachite", 44, 441, 500});
+insertAccessory({"Ring of Strength", 120, 400, 200});
+insertAccessory({"Amulet of Wisdom", 90, 600, 250});
+
+}
+
+void data1(character &charaStats) {
+    charaStats.baseATK = 500;
+    charaStats.baseDEF = 100;
+    charaStats.baseHP = 1200;
+
+    // Tambahkan data senjata
+    // Tambahkan data senjata
+insertWeapon({"Laevatein", 631});
+insertWeapon({"Excalibur", 544});
+insertWeapon({"Thunder Sword", 720});
+
+
+// Tambahkan data armor
+insertArmor({"Aquarius Imperial Armor", 250, 1606});
+insertArmor({"Aries Bustier", 370, 1133});
+insertArmor({"Knight Plate", 450, 1400});
+
+// Tambahkan data aksesori
+insertAccessory({"Euclase Earth", 188, 857, 300});
+insertAccessory({"Neptune Malachite", 44, 441, 500});
+insertAccessory({"Ring of Strength", 120, 400, 200});
+
 
 }
