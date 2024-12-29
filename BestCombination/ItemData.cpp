f548785c10d83efe1c403c27da30628aa69191fa
Ghanifhakbar <ghanifkuliahakbar@gmail.com>
2024-12-29 15:07:58 +0700

    ItemData.cpp

--- BestCombination/ItemData.cpp
+++ BestCombination/ItemData.cpp
@@ -39,29 +39,37 @@ void data1(character &charaStats) {
     charaStats.baseHP = 1200;
 
     // Tambahkan data senjata
-    insertWeapon({"Laevatein", 631});
-    insertWeapon({"Excalibur", 544});
-    insertWeapon({"Thunder Sword", 720});
-    insertWeapon({"Flame Blade", 675});
-    insertWeapon({"Shadow Dagger", 590});
-    insertWeapon({"Ice Staff", 710});
-    insertWeapon({"Phoenix Bow", 640});
+    // Tambahkan data senjata
+insertWeapon({"Laevatein", 631});
+insertWeapon({"Excalibur", 544});
+insertWeapon({"Thunder Sword", 720});
+insertWeapon({"Flame Blade", 675});
+insertWeapon({"Shadow Dagger", 590});
+insertWeapon({"Ice Staff", 710});
+insertWeapon({"Phoenix Bow", 640});
+insertWeapon({"Crystal Halberd", 680}); // Data tambahan 1
+insertWeapon({"Dragon Slayer", 750});  // Data tambahan 2
 
-    // Tambahkan data armor
-    insertArmor({"Aquarius Imperial Armor", 250, 1606});
-    insertArmor({"Aries Bustier", 370, 1133});
-    insertArmor({"Knight Plate", 450, 1400});
-    insertArmor({"Golden Shield", 320, 1230});
-    insertArmor({"Steel Breastplate", 400, 1350});
-    insertArmor({"Shadow Cloak", 290, 1100});
-    insertArmor({"Mage Robe", 330, 1000});
+// Tambahkan data armor
+insertArmor({"Aquarius Imperial Armor", 250, 1606});
+insertArmor({"Aries Bustier", 370, 1133});
+insertArmor({"Knight Plate", 450, 1400});
+insertArmor({"Golden Shield", 320, 1230});
+insertArmor({"Steel Breastplate", 400, 1350});
+insertArmor({"Shadow Cloak", 290, 1100});
+insertArmor({"Mage Robe", 330, 1000});
+insertArmor({"Celestial Armor", 500, 1800}); // Data tambahan 1
+insertArmor({"Inferno Shield", 460, 1500});  // Data tambahan 2
 
-    // Tambahkan data aksesori
-    insertAccessory({"Euclase Earth", 188, 857, 300});
-    insertAccessory({"Neptune Malachite", 44, 441, 500});
-    insertAccessory({"Ring of Strength", 120, 400, 200});
-    insertAccessory({"Amulet of Wisdom", 90, 600, 250});
-    insertAccessory({"Bracelet of Speed", 140, 300, 300});
-    insertAccessory({"Cloak of Shadows", 80, 700, 400});
-    insertAccessory({"Necklace of Power", 200, 500, 350});
+// Tambahkan data aksesori
+insertAccessory({"Euclase Earth", 188, 857, 300});
+insertAccessory({"Neptune Malachite", 44, 441, 500});
+insertAccessory({"Ring of Strength", 120, 400, 200});
+insertAccessory({"Amulet of Wisdom", 90, 600, 250});
+insertAccessory({"Bracelet of Speed", 140, 300, 300});
+insertAccessory({"Cloak of Shadows", 80, 700, 400});
+insertAccessory({"Necklace of Power", 200, 500, 350});
+insertAccessory({"Ring of Agility", 130, 450, 280}); // Data tambahan 1
+insertAccessory({"Charm of Resilience", 110, 550, 320}); // Data tambahan 2
+
 }
